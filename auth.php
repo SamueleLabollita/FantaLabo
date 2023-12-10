ef726e3665c5ce51eafd8b420b98a78b29f9d8a6
LabollitaS <101709291+SamueleLabollita@users.noreply.github.com>
2023-12-10 17:10:34 +0000

    10

--- auth.php
+++ auth.php
@@ -2,7 +2,7 @@
 session_start();
 $post = $_SESSION['POST'];
      $randomNumber = rand(1, 10);
-        
+
         if ($randomNumber % 2 == 0) {
             header("Location: /userpage.php");
         } else {
